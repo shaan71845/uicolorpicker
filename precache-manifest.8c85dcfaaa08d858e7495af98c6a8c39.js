b1420fe290c8783ff5806df122eec6bd1b3c66b5
Shaan Alam <shaanalam369@gmail.com>
2020-05-05 14:57:22 +0530

    Updates

---
+++ precache-manifest.8c85dcfaaa08d858e7495af98c6a8c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479707272defeae2476f96c05ccefeed",
+    "url": "/uicolorpicker/index.html"
+  },
+  {
+    "revision": "aef985af9a0bfcce44f2",
+    "url": "/uicolorpicker/static/css/main.aa5c3475.chunk.css"
+  },
+  {
+    "revision": "d1eacb2cd20f6ca20941",
+    "url": "/uicolorpicker/static/js/2.586a8eb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uicolorpicker/static/js/2.586a8eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef985af9a0bfcce44f2",
+    "url": "/uicolorpicker/static/js/main.c10b32ee.chunk.js"
+  },
+  {
+    "revision": "2a0e1218c645782cc1fd",
+    "url": "/uicolorpicker/static/js/runtime-main.28eed323.js"
+  }
+]);
